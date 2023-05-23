857ab356a88874e28855742943f3f6faf10d2147
Ronald-ps <104438755+Ronald-ps@users.noreply.github.com>
2023-05-23 00:16:32 -0300

    add modelos basicos (#1)
    Cria modelos de empresa, departamento e funcionários

--- core/models.py
+++ core/models.py
@@ -1,6 +1,39 @@
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+from core.soft_delete import SoftDeleteBaseModel
 
 
 class User(AbstractUser):
     pass
+
+
+class Company(SoftDeleteBaseModel):
+    cnpj = models.CharField(max_length=14, unique=True)
+    cep = models.CharField(max_length=8)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+
+class Department(SoftDeleteBaseModel):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="departments")
+    # TODO: pensar melhor sobre como deve ser esse centro de custos
+    # vou deixar esse carinha nulo por enquanto, até descobrir sobre como deve ser um centro de custos
+    cost_center = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=100)
+    integration_code = models.CharField(max_length=100)
+
+
+class Employee(SoftDeleteBaseModel):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="employees")
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="employees")
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    date_of_entry = models.DateField()
+    date_of_hire = models.DateTimeField()
+    date_of_departure = models.DateField(blank=True, null=True)
+    city = models.CharField(max_length=100)
