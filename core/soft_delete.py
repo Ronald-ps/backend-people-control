0a79809b61a73030088b1b80eca1026ec6a85943
Ronald Seabra <git.ronaldseabra@gmail.com>
2023-05-22 22:36:34 -0300

    cria logica para soft delete
    O soft delete irá modificar o campo "is_deleted" para True, ao invés
    de realmente modificar

---
+++ core/soft_delete.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        self.update(is_deleted=True)
+
+
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return SoftDeleteQuerySet(self.model, using=self._db).exclude(is_deleted=True)
+
+    def all_without_exception(self):
+        return SoftDeleteQuerySet(self.model, using=self._db)
+
+
+class SoftDeleteBaseModel(models.Model):
+    """ Base model that implements soft delete logic """
+    class Meta:
+        abstract = True
+    objects = SoftDeleteManager()
+
+    is_deleted = models.BooleanField(default=False, db_index=True)
+
+    def delete(self):
+        self.is_deleted = False
+        self.save(update_fields=["is_deleted"])
